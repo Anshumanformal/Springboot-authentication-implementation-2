178deb487ef9123d614e41d78f053cd073e8f576
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2024-10-02 13:10:56 +0000

    initial commit

---
+++ authentication_implementation/src/main/java/authentication/authentication_implementation/AuthenticationImplementationApplication.java
@@ -0,0 +1,13 @@
+package authentication.authentication_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationImplementationApplication.class, args);
+	}
+
+}
